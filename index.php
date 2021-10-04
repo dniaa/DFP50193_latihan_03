93ec4eba0c37644b7cd466a81e5493e48eb69f8d
dniaa <daniaariesya944@gmail.com>
2021-10-04 12:30:19 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 03</title>
+</head>
+<body>
+    <h1> Asas PHP </h1>
+
+    <?php
+    //komen sebaris
+    #komen sebaris
+    /*banyak baris
+    */
+
+    $namapelajar = 'Nik Dania Ariesya';
+    echo "<h2>HELLO $namapelajar </h2>"; #output
+
+    $hadir = false;
+    $umur = 20;
+    $nokp = '0133872125';
+
+    ?>
+
+</body>
+</html>
